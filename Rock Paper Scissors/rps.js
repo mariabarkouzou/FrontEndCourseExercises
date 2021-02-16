0b0131fee0892c0a7c4786041d4c4f3c981cc210
Maria Barkouzou <mbarkouzou@gmail.com>
2021-02-16 21:59:05 +0200

    Ready

--- Rock Paper Scissors/rps.js
+++ Rock Paper Scissors/rps.js
@@ -1,68 +1,82 @@
 "use strict";
 
+let playerScore = 0;
+let computerScore = 0;
+let draws = 0;
+
 /*ComputerPlay*/
 
-let round = 0;
-
 function computerPlay() {
-  const choices = [ "Rock" , "Paper" , "Scissors"];
-  const randomNumber = Math.floor(Math.random()*choices.length);
+  const choices = ["Rock", "Paper", "Scissors"];
+  const randomNumber = Math.floor(Math.random() * choices.length);
   return choices[randomNumber];
 }
-
 
 /*Single Round of Game*/
 
 function playRound(playerSelection, computerSelection) {
-
   if (playerSelection === computerSelection) {
-    return "It is a Tie!";
+    return draw;
   }
   if (
     (playerSelection === "Rock" && computerSelection === "Scissors") ||
     (playerSelection === "Scissors" && computerSelection === "Paper") ||
     (playerSelection === "Paper" && computerSelection === "Rock")
   ) {
-    return "You win! " + playerSelection + " beats " + computerSelection + "!";
+    return playerWinRound;
   }
   if (
     (computerSelection === "Rock" && playerSelection === "Scissors") ||
     (computerSelection === "Scissors" && playerSelection === "Paper") ||
     (computerSelection === "Paper" && playerSelection === "Rock")
   ) {
-    return "Computer Wins! " + computerSelection + " beats " + playerSelection + "!";
+    return computerWinRound;
   }
 }
-let playerSelection = prompt ("Do you choose Rock , Paper or Scissors?");
-  playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
-  const computerSelection = computerPlay();
 
-console.log(playRound(playerSelection, computerSelection))
+/*Round messages */
 
+let playerWinRound = "You win this round!";
+let computerWinRound = "Computer wins this round!";
+let draw = "It's a tie!";
+let playerWin = "You win the game! Way to go!";
+let computerWin = "Computer wins the game! Try again!";
 
 /*Play 5 Times */
 
-function game(){
-  let playerScore = 0;
-  let computerScore = 0;
+for (let i = 0; i < 5; i++) {
+  let playerSelection = prompt("Do you choose Rock , Paper or Scissors?");
+  playerSelection =
+    playerSelection.slice(0, 1).toUpperCase() +
+    playerSelection.slice(1, playerSelection.length).toLowerCase();
+  const computerSelection = computerPlay();
+  let roundResult = playRound(playerSelection, computerSelection);
+  console.log(roundResult);
+  gameScore(roundResult);
+  console.log("Your score is " + playerScore);
+  console.log("The computer's score is " + computerScore);
 
-  for ( let i=0; i < 5; i++ ){
-
-if (playerScore < computerScore){
-  console.log("You lose the game.");
-} else if (playerScore > computerScore) {
-  console.log("You win the game");
-} else {
-  console.log("You are tied");
+  if (playerScore === 5 || computerScore === 5) {
+    break;
+  }
 }
+
+/*Keeping Score */
+
+function gameScore(result) {
+  if (result === playerWinRound) {
+    playerScore++;
+  } else if (result === draw) {
+    draws++;
+  } else {
+    computerScore++;
+  }
+
+  if (playerScore === 5) {
+    console.log(playerWin);
+  }
+
+  if (computerScore === 5) {
+    console.log(computerWin);
+  }
 }
-}
-
-
-
-
-
-
-
-    
-  
