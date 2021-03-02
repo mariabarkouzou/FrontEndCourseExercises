0cea690457f8b69258eb99fa739f76a71923aee3
Maria Barkouzou <mbarkouzou@gmail.com>
2021-03-02 13:59:30 +0200

    Refactored Game and added images

--- Rock Paper Scissors/rps.js
+++ Rock Paper Scissors/rps.js
@@ -1,82 +1,91 @@
-"use strict";
+let compChoice = {Value: ""};
+let playerChoice;
+let compChoiceInt = 0;
+let playerChoiceInt = 0;
+const buttons = document.querySelectorAll('.btn');
 
 let playerScore = 0;
-let computerScore = 0;
-let draws = 0;
+let compScore = 0;
 
-/*ComputerPlay*/
+const player = document.querySelector("#player-score");
+player.textContent = `Player Score: ${playerScore}`;
 
-function computerPlay() {
-  const choices = ["Rock", "Paper", "Scissors"];
-  const randomNumber = Math.floor(Math.random() * choices.length);
-  return choices[randomNumber];
+const computer = document.querySelector("#comp-score");
+computer.textContent = `Computer Score: ${compScore}`;
+
+const output = document.querySelector("#output");
+output.textContent = "May the Best Win!🤞"
+
+
+buttons.forEach((button)=>{button.addEventListener('click',()=>{
+
+    playerChoice = button.id;
+    if (playerChoice == "rock"){
+        playerChoiceInt = 0;
+    }
+    else if (playerChoice == "paper"){
+        playerChoiceInt = 1;
+    }
+    else if (playerChoice == "scissors")
+    {
+        playerChoiceInt = 2;
+    }
+    compChoiceInt = computerPlay(compChoice);
+    playGame();
+    })
+
+})
+
+function computerPlay(compChoice){
+    let choiceNum = Math.floor(Math.random() * 3);
+    if (choiceNum == 0){
+        compChoice.Value = "rock";
+    }
+    else if (choiceNum == 1){
+        compChoice.Value = "paper";
+    }
+    else if(choiceNum == 2){
+        compChoice.Value = "scissors";
+    }
+    return choiceNum;
 }
 
-/*Single Round of Game*/
-
-function playRound(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection) {
-    return draw;
-  }
-  if (
-    (playerSelection === "Rock" && computerSelection === "Scissors") ||
-    (playerSelection === "Scissors" && computerSelection === "Paper") ||
-    (playerSelection === "Paper" && computerSelection === "Rock")
-  ) {
-    return playerWinRound;
-  }
-  if (
-    (computerSelection === "Rock" && playerSelection === "Scissors") ||
-    (computerSelection === "Scissors" && playerSelection === "Paper") ||
-    (computerSelection === "Paper" && playerSelection === "Rock")
-  ) {
-    return computerWinRound;
-  }
-}
-
-/*Round messages */
-
-let playerWinRound = "You win this round!";
-let computerWinRound = "Computer wins this round!";
-let draw = "It's a tie!";
-let playerWin = "You win the game! Way to go!";
-let computerWin = "Computer wins the game! Try again!";
-
-/*Play 5 Times */
-
-for (let i = 0; i < 5; i++) {
-  let playerSelection = prompt("Do you choose Rock , Paper or Scissors?");
-  playerSelection =
-    playerSelection.slice(0, 1).toUpperCase() +
-    playerSelection.slice(1, playerSelection.length).toLowerCase();
-  const computerSelection = computerPlay();
-  let roundResult = playRound(playerSelection, computerSelection);
-  console.log(roundResult);
-  gameScore(roundResult);
-  console.log("Your score is " + playerScore);
-  console.log("The computer's score is " + computerScore);
-
-  if (playerScore === 5 || computerScore === 5) {
-    break;
-  }
-}
-
-/*Keeping Score */
-
-function gameScore(result) {
-  if (result === playerWinRound) {
+function playRound(){
+        let win_array = [[0, 2, 1], 
+                        [1, 0, 2], 
+                        [2, 1, 0]];
+    let result = win_array[playerChoiceInt][compChoiceInt];
+    if (result == 0){
+    output.textContent = `It's a tie! You chose ${playerChoice} & the computer chose ${compChoice.Value}!`;
+    }
+    else if (result == 1){
+    output.textContent = `You won! You chose ${playerChoice} & the computer chose ${compChoice.Value}!`;
     playerScore++;
-  } else if (result === draw) {
-    draws++;
-  } else {
-    computerScore++;
-  }
 
-  if (playerScore === 5) {
-    console.log(playerWin);
-  }
-
-  if (computerScore === 5) {
-    console.log(computerWin);
-  }
+    }
+    else if (result == 2){
+    output.textContent = `You lost! You chose ${playerChoice} & the computer chose ${compChoice.Value}!`;
+    compScore++;
+    }
 }
+
+function playGame(){
+        output.textContent = "Choose Rock, Paper, or Scissors";
+        playRound();
+        player.textContent = `Player Score: ${playerScore}`;
+        computer.textContent = `Computer Score: ${compScore}`;
+        if (playerScore == 5){
+            output.textContent = "You Won the Game!🔥";
+            playerScore = 0;
+            compScore = 0;
+            player.textContent = `Player Score: ${playerScore}`;
+            computer.textContent = `Computer Score: ${compScore}`;
+        }
+        else if (compScore == 5){
+            output.textContent = "You lost the game! Try Again!"
+            playerScore = 0;
+            compScore = 0;
+            player.textContent = `Player Score: ${playerScore}`;
+            computer.textContent = `Computer Score: ${compScore}`;
+        }
+};
