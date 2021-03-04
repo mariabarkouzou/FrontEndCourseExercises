9b71cec9854670c35af13e3262d9f6c6362e0b56
Maria Barkouzou <mbarkouzou@gmail.com>
2021-03-04 23:50:43 +0200

    Exercise fibonacci ready

--- Fundamentals 5/fibonacci/fibonacci.js
+++ Fundamentals 5/fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  let arr = [0, 1];
 
-}
+  if (n > 0) {
+    for (let i = 2; i < n + 1; i++) {
+      arr.push(arr[i - 2] + arr[i - 1]);
+    }
+    return arr[n];
+  } else {
+    return "OOPS";
+  }
+};
 
-module.exports = fibonacci
+module.exports = fibonacci;
