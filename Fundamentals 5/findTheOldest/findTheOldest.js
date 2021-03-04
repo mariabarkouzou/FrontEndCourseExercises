682494d5b07eeb4c49bd080c58ae5eb8c3b09063
Maria Barkouzou <mbarkouzou@gmail.com>
2021-03-05 00:31:01 +0200

    Exercise findTheOldest ready

--- Fundamentals 5/findTheOldest/findTheOldest.js
+++ Fundamentals 5/findTheOldest/findTheOldest.js
@@ -1,6 +1,8 @@
-let findTheOldest = function() {
+let findTheOldest = function(array) {
+ array.sort((a, b) => a.yearOfBirth > b.yearOfBirth ? 1 : -1 );
+return array[0];
 
-}
+}; 
 
 module.exports = findTheOldest
 
