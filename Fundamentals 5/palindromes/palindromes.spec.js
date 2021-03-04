263fe2049045027b182bf363aba3c8611e2fb1bb
Maria Barkouzou <mbarkouzou@gmail.com>
2021-03-05 00:54:49 +0200

    Exercise palindromes ready

--- Fundamentals 5/palindromes/palindromes.spec.js
+++ Fundamentals 5/palindromes/palindromes.spec.js
@@ -4,19 +4,19 @@ describe('palindromes', function() {
   it('works with single words', function() {
     expect(palindromes('racecar')).toEqual(true);
   });
-  xit('works with punctuation ', function() {
+ it('works with punctuation ', function() {
     expect(palindromes('racecar!')).toEqual(true);
   });
-  xit('works with upper-case letters ', function() {
+  it('works with upper-case letters ', function() {
     expect(palindromes('Racecar!')).toEqual(true);
   });
-  xit('works with multiple words', function() {
+  it('works with multiple words', function() {
     expect(palindromes('A car, a man, a maraca.')).toEqual(true);
   });
-  xit('works with multiple words', function() {
+  it('works with multiple words', function() {
     expect(palindromes('Animal loots foliated detail of stool lamina.')).toEqual(true);
   });
-  xit('doesn\'t just always return true', function() {
+  it('doesn\'t just always return true', function() {
     expect(palindromes('ZZZZ car, a man, a maraca.')).toEqual(false);
   });
 
