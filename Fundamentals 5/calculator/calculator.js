814a14fbd0d01e333e19358c3aafd5a1b0e7d8be
Maria Barkouzou <mbarkouzou@gmail.com>
2021-03-04 23:26:54 +0200

    Exercise calculator ready

--- Fundamentals 5/calculator/calculator.js
+++ Fundamentals 5/calculator/calculator.js
@@ -1,32 +1,43 @@
-function add () {
-	
+function add(a, b) {
+  return a + b;
 }
 
-function subtract () {
-	
+function subtract(a, b) {
+  return a - b;
 }
 
-function sum () {
-	
+function sum(array) {
+  return array.reduce((total, num) => {
+    return total + num;
+  }, 0);
 }
 
-function multiply () {
-	
+function multiply(array) {
+	return array.reduce((total, num) => {
+		return total * num;
+	  }, 1);
+	}
+
+
+
+function power(a , b) {
+	return a ** b;
 }
 
-function power() {
-	
-}
-
-function factorial() {
-	
-}
+function factorial(num) {
+	if (num === 0 || num === 1)
+	  return 1;
+	for (let i = num - 1; i >= 1; i--) {
+	  num *= i;
+	}
+	return num;
+  }
 
 module.exports = {
-	add,
-	subtract,
-	sum,
-	multiply,
-    power,
-	factorial
-}
+  add,
+  subtract,
+  sum,
+  multiply,
+  power,
+  factorial,
+};
