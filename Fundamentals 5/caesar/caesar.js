1ab076431ab2bd726fc23643e12bd6d02ebe1286
Maria Barkouzou <mbarkouzou@gmail.com>
2021-03-06 20:29:04 +0200

    Exercise caesar ready

--- Fundamentals 5/caesar/caesar.js
+++ Fundamentals 5/caesar/caesar.js
@@ -1,5 +1,35 @@
-const caesar = function() {
+const caesar = function (userInput, shiftNumber) {
+  let result = "";
+  for (let i = 0; i < userInput.length; i++) {
 
-}
+    //Negative Numbers
 
-module.exports = caesar
+    if (shiftNumber < 0) {
+        shiftNumber = 26 + (shiftNumber % 26);
+      } 
+
+    //UpperCase letters
+
+     if (userInput.charCodeAt(i) >= 65 && userInput.charCodeAt(i) <= 90) {
+      const charCode = userInput.charCodeAt(i);
+      result += String.fromCharCode((charCode - 65 + shiftNumber) % 26 + 65); 
+
+      // LowerCase letters
+
+    } else if  (userInput.charCodeAt(i) >= 97 && userInput.charCodeAt(i) <= 122) {
+      const charCode = userInput.charCodeAt(i);
+      result += String.fromCharCode((charCode - 97 + shiftNumber) % 26 + 97); 
+
+      // If it is not a letter let it pass
+    } else {
+        result += userInput.charAt(i);
+
+    }
+
+  }
+  return result;
+  
+};
+
+module.exports = caesar;
+
