98e726731762660470d5c470ad6af8adc7306a98
Maria Barkouzou <mbarkouzou@gmail.com>
2021-04-19 11:24:42 +0300

    Exercise Ready

---
+++ JavaScript/Objects and Object Constructors/exercise.js
@@ -0,0 +1,13 @@
+function book(title, author,pages,read) {
+    this.title= title
+    this.author = author
+    this.pages = pages
+    this.read = read    
+    this.info = function() { 
+        return `"${title} by ${author} , ${pages} pages  , ${read}." `
+    }
+  }
+  
+  let theHobbit = new book("The Hobbit" , "J.R.R. Tolkien" , "295" , "not read yet");
+  theHobbit.info()
+  console.log(theHobbit.info())
