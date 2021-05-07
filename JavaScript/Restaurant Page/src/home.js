1c59ee9d7992aa71a6207d0d88a8e5f2fd096229
Maria Barkouzou <mbarkouzou@gmail.com>
2021-05-07 13:04:29 +0300

    Almost ready , needs some css changes

---
+++ JavaScript/Restaurant Page/src/home.js
@@ -0,0 +1,29 @@
+function createHome() {
+    const home = document.createElement("div");
+    home.classList.add("home");
+  
+    const homeImage = document.createElement("img");
+    homeImage.src = "images/home.jpg";
+    homeImage.alt = "Spices";
+  
+    home.appendChild(createParagraph("Spain's most famous place!"));
+    home.appendChild(createParagraph("With you since 1982"));
+    home.appendChild(homeImage);
+    home.appendChild(createParagraph("Order online or visit us!"));
+  
+    return home;
+  }
+  
+  function createParagraph(text) {
+    const paragraph = document.createElement("p");
+    paragraph.textContent = text;
+    return paragraph;
+  }
+  
+  function loadHome() {
+    const main = document.getElementById("main");
+    main.textContent = "";
+    main.appendChild(createHome());
+  }
+  
+  export default loadHome;
