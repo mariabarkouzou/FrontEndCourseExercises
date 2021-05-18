93be48470af0b80b9126bd21bf7733bb612f689e
Maria Barkouzou <mbarkouzou@gmail.com>
2021-05-18 09:29:53 +0300

    DropDownMenu

---
+++ JavaScript/User-Interface/DropDownMenu/index.js
@@ -0,0 +1,25 @@
+let a_parent =  document.querySelectorAll(".a_parent");
+let dd_menu_a = document.querySelectorAll(".dd_menu_a");
+
+a_parent.forEach(function(aitem){
+
+		aitem.addEventListener("click", function(){
+			a_parent.forEach(function(aitem){
+				aitem.classList.remove("active");
+			})
+			dd_menu_a.forEach(function(dd_menu_item){
+				dd_menu_item.classList.remove("active");
+			})
+			aitem.classList.add("active");
+		})
+})
+
+dd_menu_a.forEach(function(dd_menu_item){
+
+		dd_menu_item.addEventListener("click", function(){
+			dd_menu_a.forEach(function(dd_menu_item){
+				dd_menu_item.classList.remove("active");
+			})
+			dd_menu_item.classList.add("active");
+		})
+})
