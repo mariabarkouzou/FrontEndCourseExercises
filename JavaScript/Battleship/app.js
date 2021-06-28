1d91829f33df96529570da8f9c3ddf05b8f34821
Maria Barkouzou <mbarkouzou@gmail.com>
2021-06-28 09:22:07 +0300

    Rotate ships function ready

--- JavaScript/Battleship/app.js
+++ JavaScript/Battleship/app.js
@@ -14,88 +14,121 @@ document.addEventListener("DOMContentLoaded", () => {
   const infoDisplay = document.querySelector("#info");
   const userSquares = [];
   const computerSquares = [];
+  let isHorizontal = true;
   const width = 10;
+
   // Create board
 
-  function createBoard(grid , squares) {
+  function createBoard(grid, squares) {
     for (let i = 0; i < width * width; i++) {
       const square = document.createElement("div");
-     square.dataset.id = i;
-     grid.appendChild(square);
-     squares.push(square);
+      square.dataset.id = i;
+      grid.appendChild(square);
+      squares.push(square);
     }
   }
-createBoard(userGrid , userSquares);
-createBoard(computerGrid , computerSquares);
+  createBoard(userGrid, userSquares);
+  createBoard(computerGrid, computerSquares);
 
+  //Ships
 
-//Ships
+  const shipArray = [
+    {
+      name: "destroyer",
+      directions: [
+        [0, 1],
+        [0, width],
+      ],
+    },
+    {
+      name: "submarine",
+      directions: [
+        [0, 1, 2],
+        [0, width, width * 2],
+      ],
+    },
+    {
+      name: "cruiser",
+      directions: [
+        [0, 1, 2],
+        [0, width, width * 2],
+      ],
+    },
+    {
+      name: "battleship",
+      directions: [
+        [0, 1, 2, 3],
+        [0, width, width * 2, width * 3],
+      ],
+    },
+    {
+      name: "carrier",
+      directions: [
+        [0, 1, 2, 3, 4],
+        [0, width, width * 2, width * 3, width * 4],
+      ],
+    },
+  ];
 
-const shipArray = [
-  {
-  name: "destroyer" , 
-  directions: [
-    [0,1],
-    [0,width]
-  ]},
-{
-  name: "submarine",
-  directions: [
-    [0,1,2],
-    [0,width,width*2]
-  ]
-},
-{
-  name: "cruiser",
-  directions: [
-    [0,1,2],
-    [0,width,width*2]
-  ]
-},
-{
-  name: "battleship",
-  directions: [
-    [0,1,2,3],
-    [0,width,width*2,width*3]
-  ]
-},
-{
-  name: "carrier",
-  directions: [
-    [0,1,2,3,4],
-    [0,width,width*2,width*3,width*4]
-  ]
-},]
+  //Draw the computers ships in random locations
 
+  function generateShip(ship) {
+    let randomDirection = Math.floor(Math.random() * ship.directions.length);
+    let current = ship.directions[randomDirection];
 
-//Draw the computers ships in random locations
+    if (randomDirection === 0) direction = 1;
+    if (randomDirection === 1) direction = 10;
+    let randomStart = Math.abs(
+      Math.floor(
+        Math.random() * computerSquares.length -
+          ship.directions[0].length * direction
+      )
+    );
 
-function generateShip(ship){
-  let randomDirection = Math.floor(Math.random() * ship.directions.length);
-  let current = ship.directions[randomDirection]
+    const isTaken = current.some((index) =>
+      computerSquares[randomStart + index].classList.contains("taken")
+    );
+    const isAtRightEdge = current.some(
+      (index) => (randomStart + index) % width === width - 1
+    );
+    const isAtLeftEdge = current.some(
+      (index) => (randomStart + index) % width === 0
+    );
 
-  if(randomDirection === 0) direction = 1
-  if(randomDirection === 1) direction = 10
-  let randomStart = Math.abs(Math.floor(Math.random() * computerSquares.length - (ship.directions[0].length * direction)))
+    if (!isTaken && !isAtRightEdge && !isAtLeftEdge)
+      current.forEach((index) =>
+        computerSquares[randomStart + index].classList.add("taken", ship.name)
+      );
+    else generateShip(ship);
+  }
 
-const isTaken = current.some(index => computerSquares[randomStart + index].classList.contains("taken"))
-const isAtRightEdge = current.some(index =>(randomStart + index) % width === width - 1)
-const isAtLeftEdge = current.some(index => (randomStart + index) % width ===0)
+  generateShip(shipArray[0]);
+  generateShip(shipArray[1]);
+  generateShip(shipArray[2]);
+  generateShip(shipArray[3]);
+  generateShip(shipArray[4]);
 
-if (!isTaken && !isAtRightEdge && !isAtLeftEdge) current.forEach(index => computerSquares[randomStart + index].classList.add("taken" , ship.name))
-else generateShip(ship)
-}
+  //Rotate The Ships
 
-generateShip(shipArray[0])
-generateShip(shipArray[1])
-generateShip(shipArray[2])
-generateShip(shipArray[3])
-generateShip(shipArray[4])
-
-//Rotate The Ships
-
-
-
-
-
+  function rotateShip() {
+    if (isHorizontal) {
+      destroyer.classList.toggle("destroyer-container-vertical");
+      submarine.classList.toggle("submarine-container-vertical");
+      cruiser.classList.toggle("cruiser-container-vertical");
+      battleship.classList.toggle("battleship-container-vertical");
+      carrier.classList.toggle("carrier-container-vertical");
+      isHorizontal = false;
+      return;
+    }
+    if (!isHorizontal) {
+      destroyer.classList.toggle("destroyer-container-vertical");
+      submarine.classList.toggle("submarine-container-vertical");
+      cruiser.classList.toggle("cruiser-container-vertical");
+      battleship.classList.toggle("battleship-container-vertical");
+      carrier.classList.toggle("carrier-container-vertical");
+      isHorizontal = true;
+      return;
+    }
+  }
+  rotateBtn.addEventListener("click", rotateShip);
 });
